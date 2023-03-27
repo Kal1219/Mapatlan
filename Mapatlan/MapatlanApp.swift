716601cf8b237e6a29c8f466a2c8f9ed5cd1b503
Juan Genaro Garcia Alanis <ga.juangenaro@gmail.com>
2023-03-27 13:55:51 -0600

    Initial Commit

---
+++ Mapatlan/MapatlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapatlanApp.swift
+//  Mapatlan
+//
+//  Created by ServicioMapatlan on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MapatlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
